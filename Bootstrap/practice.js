215ec9f7f9eaf65d23e1bcf39593ca705f3fcf91
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-07-30 12:51:18 +0530

    practice

--- Bootstrap/practice.js
+++ Bootstrap/practice.js
@@ -1,16 +1,32 @@
 // Traffice light
 let light = 'Red';
-if(light = "Red"){
+if(light == "Red"){
     console.log("Stop!");
 }
-else if(light = "Yellow"){
+else if(light == "Yellow"){
     console.log("Slow down!");
 }
-else if(light = "Green"){
+else if(light == "Green"){
     console.log("Go!");
 }
 else{
     console.log("Traffic light is not working!");
 }
 
-// 
+// Popcorn price
+let size = 'XS';
+if(size == 'XL'){
+    console.log("Price is Rs. 250");
+}
+else if(size == 'L'){
+    console.log("Price is Rs. 200");
+}
+else if(size == 'M'){
+    console.log("Price is Rs. 100");
+}
+else if(size == 'S'){
+    console.log('Price is Rs. 50');
+}
+else{
+    console.log("Size is not applicable!")
+}
