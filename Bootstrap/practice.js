4f6ea571f1aed984cd01acf54c60308f136f437b
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-07-30 12:47:23 +0530

    js

---
+++ Bootstrap/practice.js
@@ -0,0 +1,16 @@
+// Traffice light
+let light = 'Red';
+if(light = "Red"){
+    console.log("Stop!");
+}
+else if(light = "Yellow"){
+    console.log("Slow down!");
+}
+else if(light = "Green"){
+    console.log("Go!");
+}
+else{
+    console.log("Traffic light is not working!");
+}
+
+// 
