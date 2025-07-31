083b5f693fc695c24baf10b64d215a4eb23098f6
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-07-31 19:32:44 +0530

    array

---
+++ JS/array.js
@@ -0,0 +1,9 @@
+let cars = ["audi","bmw","maruti"];
+cars.push("xuv");
+console.log(cars);
+cars.pop();
+console.log(cars);
+cars.unshift("swift");
+console.log(cars);
+cars.shift();
+console.log(cars);
