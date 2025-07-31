e31508be375d0cd77880836287b1e32110a34485
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-07-31 19:43:27 +0530

    practice

--- JS/array.js
+++ JS/array.js
@@ -18,3 +18,12 @@ months.unshift("june");
 console.log(months);
 months.unshift("july");
 console.log(months);
+
+// indexOf and includes method
+console.log(months.indexOf("july"));
+console.log(months.indexOf("July"));
+console.log(months.indexOf("april"));
+
+console.log(cars.includes("bmw"));
+console.log(cars.includes("Bmw"));
+console.log(cars.includes("thar"));
