3fdef8c6d5512639d8f937eb288ff93afc49134c
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-08-14 19:07:29 +0530

    nested loop

--- JS/loop.js
+++ JS/loop.js
@@ -39,3 +39,11 @@ n = parseInt(n);
 for(let i=n; i<=n*10; i=i+n){
     console.log(i);
 }
+
+console.log("nested for loop");
+for(let i=1; i<=3; i++){
+    console.log(`outer loop ${i}`);
+    for(let j=1; j<=3; j++){
+        console.log(j);
+    }
+}
