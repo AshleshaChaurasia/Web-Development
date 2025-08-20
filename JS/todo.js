891f978184d9d5b4470599b0ada121895c6ba80f
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-08-20 15:03:36 +0530

    todo

---
+++ JS/todo.js
@@ -0,0 +1,30 @@
+let todo = [];
+let req = prompt("Please enter your request");
+
+while(true){
+    if(req == "quit"){
+        console.log("quitting app");
+        break;
+    }
+
+    if(req == "list"){
+        console.log("----------");
+        for(let i=0; i<todo.length; i++){
+            console.log(i, todo[i]);
+        }
+        console.log("----------");
+    }
+    else if(req=="add"){
+        let task = prompt("Please enter the task you want to enter");
+        todo.push(task);
+        console.log("task added");
+    }
+
+    req = prompt("Please enter your request");
+
+    else if(req=="delete"){
+        let idx = prompt("Please enter the task index you want to delete");
+        todo.splice(idx, 1);
+        console.log("task deleted");
+    }
+}
