6f14ef2e4eab64c2ae699f6d9d6dfc0819f38864
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-08-28 00:51:48 +0530

    game

--- simon.js
+++ simon.js
@@ -18,6 +18,7 @@ document.addEventListener("keypress", function(){
 })
 
 function levelUp(){
+    userSeq =[];
     level++;
     h2.innerText = `Level ${level}`;
 
@@ -26,12 +27,66 @@ function levelUp(){
     let randomColor = btns[randomInd];
     let randomBtn = document.querySelector(`.${randomColor}`);
 
-    btnFlash(randomBtn);
+    gameSeq.push(randomColor);
+    // console.log(gameSeq);
+
+    gameFlash(randomBtn);
 }
 
-function btnFlash(btn){
+function checkAns(idx){
+    // console.log("Current level : ", level);
+
+    if(userSeq[idx] === gameSeq[idx]){
+        // console.log("same value");
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+        }
+    }else{
+        h2.innerHTML = `Game over! Your score was <b>${level}</b> <br> Press any key to start`
+
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(()=>{
+            document.querySelector("body").style.backgroundColor = "white";
+        },250);
+
+        reset();
+    }
+}
+
+function gameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
     },250);
+}
+
+function userFlash(btn){
+    btn.classList.add("userFlash");
+    setTimeout(function(){
+        btn.classList.remove("userFlash");
+    },250);
+}
+
+function btnPress(){
+    let btn = this;
+    // console.log(this);
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    // console.log(userColor);
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress);
+}
+
+function reset(){
+    started = false;
+    gameSeq=[];
+    userSeq=[];
+    level = 0;
 }
