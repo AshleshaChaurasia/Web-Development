275bc55687dbbae930ddcdffea87c4ba46fe80c2
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-08-31 23:58:12 +0530

    express

--- Express/index.js
+++ Express/index.js
@@ -7,16 +7,36 @@ let port = 3000;
 
 app.listen(port, ()=>{
     console.log(`app is listening on port : ${port}`);
-})
+});
 
-app.use((req, res) => {
-    // console.log(req);
-    console.log("request recieved");
-    // res.send("This is a basic response!");
-    // res.send({
-    //     name:"apple",
-    //     color:"red"
-    // })
-    let code = "<h1>Fruits</h1><ul><li>apple</li><li>orange</li></ul>";
-    res.send(code);
+// app.use((req, res) => {
+//     // console.log(req);
+//     console.log("request recieved");
+//     // res.send("This is a basic response!");
+//     // res.send({
+//     //     name:"apple",
+//     //     color:"red"
+//     // })
+//     let code = "<h1>Fruits</h1><ul><li>apple</li><li>orange</li></ul>";
+//     res.send(code);
+// });
+
+app.get("/",(req, res) => {
+    res.send("You contacted root path");
+});
+
+app.get("/apple", (req, res)=> {
+    res.send("You contacted apple path");
+});
+
+app.get("/orange",(req,res)=>{
+    res.send("You contacted orange path");
+});
+
+// app.get( '*' , (req, res) => {
+//     res.send("This path does not exist");
+// });
+
+app.post("/",(req, res) => {
+    res.send("You sent a post request to root path");
 });
