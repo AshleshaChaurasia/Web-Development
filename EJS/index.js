da159e16268815a9a778bdf8baf3013a8f370fa0
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-09-02 00:19:29 +0530

    ejs

--- EJS/index.js
+++ EJS/index.js
@@ -3,7 +3,11 @@ const app = express();
 
 const port = 8080;
 
+const path = require("path");
+
 app.set("view engine", "ejs");
+
+app.set("views",path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
