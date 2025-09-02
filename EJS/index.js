6b9e99c372b22e3e4d09b45a5b2973bb0d3ba3c6
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-09-02 23:45:27 +0530

    loops

--- EJS/index.js
+++ EJS/index.js
@@ -11,17 +11,23 @@ app.set("views",path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
-})
+});
 
 app.get("/hello", (req, res) => {
     res.send("hello");
-})
+});
 
 app.get("/rolldice", (req, res) => {
     let diceVal = Math.floor(Math.random()*6)+1;
     res.render("rolldice.ejs", {num : diceVal});
-})
+});
+
+app.get("/ig/:username",(req, res) => {
+    const followers =["adam","steve","bob","abc"];
+    let {username} = req.params;
+    res.render("instagram.ejs",{username, followers});
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-})
+});
