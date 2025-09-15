8a3c2795f637a80da871025163f248f3dd2652e6
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-09-15 13:17:46 +0530

    sql

--- SQL/index.js
+++ SQL/index.js
@@ -1,4 +1,31 @@
 const {faker} = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "app",
+    password:"1234"
+});
+
+// let q = "SHOW TABLES";
+let q = "INSERT INTO user(id, username, email, password) VALUES ?";
+let user = ["123", "123_newuser", "abc@gmail.com", "abc"];
+let users = [["123b", "123_newuserb", "abc@gmail.comb", "abcb"],["123c", "123_newuserc", "abc@gmail.comc", "abcc"]];
+
+try{
+    connection.query(q, [users], (err,result) => {
+        if(err) throw err;
+        console.log(result);
+        // console.log(result.length);
+        // console.log(result[0]);
+        // console.log(result[1]);
+    });
+} catch (err){
+    console.log(err);
+}
+
+connection.end();
 
 let getRandomUser = () => {
     return{
@@ -9,4 +36,4 @@ let getRandomUser = () => {
     };
 };
 
-console.log(getRandomUser());
+// console.log(getRandomUser());
