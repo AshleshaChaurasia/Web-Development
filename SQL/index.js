11f4ade8f4856108a0eeb288431eecc5f9d444fb
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-09-17 23:44:20 +0530

    mongo

--- SQL/index.js
+++ SQL/index.js
@@ -2,6 +2,13 @@ const {faker} = require("@faker-js/faker");
 const mysql = require("mysql2");
 const express = require("express");
 const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"/views"));
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -33,20 +40,81 @@ let getRandomUser = () => {
 // let user = ["123", "123_newuser", "abc@gmail.com", "abc"];
 // let users = [["123b", "123_newuserb", "abc@gmail.comb", "abcb"],["123c", "123_newuserc", "abc@gmail.comc", "abcc"]];
 
-
+//Home route
 app.get("/", (req, res) => {
     let q = "SELECT count(*) FROM user";
     try{
         connection.query(q, (err, result) => {
             if (err) throw err;
-            console.log(result[0]["count(*)"]);
-            res.send(result[0]["count(*)"]);
+            let count = result[0]["count(*)"];
+            console.log(count);
+            res.render("home.ejs", {count});
         });
     }catch(err){
         console.log(err);
-        res.send("Somme error in DB");
+        res.send("Some error in DB");
     }
 });
+
+//Show all users route
+app.get("/user", (req, res) => {
+    let q = "SELECT * FROM user";
+    try{
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let users = result;
+            // console.log(users);
+            res.render("user.ejs", {users});
+        });
+    }catch(err){
+        res.send("Some error in DB");
+    }
+});
+
+//Edit username route
+app.get("/user/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let q = `SELECT * FROM user WHERE id="${id}"`;
+
+    try{
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            console.log(user);
+            res.render("edit.ejs", {user});
+        });
+    }catch(err){
+        res.send("Some error in DB");
+    }
+});
+
+//Update (DB) Route
+app.patch("/user/:id", (req, res) => {
+    let {id} = req.params;
+    let {password: formPass, username: newUsername} = req.body;
+    let q = `SELECT * FROM user WHERE id="${id}"`;
+
+    try{
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            if(formPass != user.password){
+                res.send("WRONG Password");
+            }else{
+                let q2 = `UPDATE user SET username="${newUsername}" WHERE id="${id}"`;
+                connection.query(q2, (err, result) => {
+                    if (err) throw err;
+                    res.redirect("/user");
+                })
+            }
+        });
+    }catch(err){
+        res.send("Some error in DB");
+    }
+});
+
+//Add user
+//Delete user (correct email id and password)
 
 app.listen("8080", () => {
     console.log("Server is listening to port 8080");
