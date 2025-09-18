7bf891cf8aff7632c222cd07cd1e00887b8c223e
AshleshaChaurasia <ashleshachaurasia1511@gmail.com>
2025-09-18 13:49:26 +0530

    mongoose

--- Mongo/index.js
+++ Mongo/index.js
@@ -15,5 +15,26 @@ const userSchema = new mongoose.Schema({
     age: Number
 });
 
-// const User = mongoose.model("User", userSchema);
-const Employee = mongoose.model("Employee", userSchema);
+const User = mongoose.model("User", userSchema);
+// const Employee = mongoose.model("Employee", userSchema);
+
+// let user1 = new User({
+//     name: "Ashlesha",
+//     email:"ash@yahoo.in",
+//     age:23
+// });
+
+// let user2 = new User({
+//     name: "Gaurav",
+//     email:"gauri@gmail.com",
+//     age:22
+// });
+
+// user1.save();
+// user2.save().then((res)=>{console.log(res)}).catch(err=>{console.log(err)});
+
+User.insertMany([
+    {name:"Manju", email:"manju@gmail.com", age:54},
+    {name:"Sanjay",email:"sanju5@yahoo.in",age:55},
+    {name:"Babulal",email:"babu123@gmail.com",age:69}
+]).then(res=>{console.log(res)});
